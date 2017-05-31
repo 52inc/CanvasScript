a789ab5499f3ff2dec3bd848afe79fe4f6c43fbd
Drew Heavner <veedubusc@gmail.com>
2017-05-31 10:29:29 -0400

    Added kotlin extensions library module

--- library-kotlin/src/test/java/com/ftinc/canvasscript/kotlin/ExampleUnitTest.java
+++ library-kotlin/src/test/java/com/ftinc/canvasscript/kotlin/ExampleUnitTest.java
@@ -13,4 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-include ':sample', ':library', ':library-kotlin'
+
+package com.ftinc.canvasscript.kotlin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
